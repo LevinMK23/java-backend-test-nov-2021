8c06de033ef1d5c645d636294f8fabe26c6ffce8
LEV1970 <80787604+LEV1970@users.noreply.github.com>
2021-11-20 15:46:12 +0300

    HomeWork4

--- ImgurApiTest.java
+++ ImgurApiTest.java
@@ -1,21 +1,36 @@
 package com.geekbrains.restApi.imgur;
 
 import io.restassured.RestAssured;
+import io.restassured.builder.RequestSpecBuilder;
+import io.restassured.builder.ResponseSpecBuilder;
+import io.restassured.specification.RequestSpecification;
+import io.restassured.specification.ResponseSpecification;
 import org.junit.jupiter.api.*;
 
 
 import static com.geekbrains.restApi.imgur.ImgurApiParams.FILE_URL;
+import static io.restassured.RestAssured.*;
 import static org.hamcrest.Matchers.notNullValue;
 import static org.hamcrest.core.Is.is;
-import static io.restassured.RestAssured.given;
 
 @TestMethodOrder(MethodOrderer.OrderAnnotation.class)
 public class ImgurApiTest {
 
-
     @BeforeAll
     static void beforeAll() {
         RestAssured.baseURI = ImgurApiParams.API_URL + "/" + ImgurApiParams.API_VERSION;
+
+        requestSpecification = new RequestSpecBuilder()
+                .setAuth(oauth2(ImgurApiParams.TOKEN))
+                .build();
+
+        responseSpecification = new ResponseSpecBuilder()
+                .expectBody("data", is(notNullValue()))
+                .expectBody("success", is(true))
+                .expectStatusLine("HTTP/1.1 200 OK")
+                .expectContentType("application/json")
+                .expectStatusCode(200)
+                .build();
     }
 
     @DisplayName("Тест на получение базовой информации об аккаунте")
@@ -23,21 +38,19 @@ public class ImgurApiTest {
     @Order(1)
     void testAccountBase() {
         String url = "/account/" + "LShubin";
+        ResponseSpecification responseSpecification = new ResponseSpecBuilder()
+                .expectBody("data.url", is("LShubin"))
+                .expectBody("data.reputation", is(0))
+                .build();
         given().when()
-                .auth()
-                .oauth2(ImgurApiParams.TOKEN)
+                .spec(requestSpecification)
                 .log()
                 .all()
                 .expect()
-                .statusCode(is(200))
-                .body("success", is(true))
-                .body("status", is(200))
-                .body("data.url", is("LShubin"))
-                .body("data.reputation", is(0))
-                .log()
-                .all()
+                .spec(responseSpecification)
                 .when()
-                .get(url);
+                .get(url)
+                .prettyPeek();
     }
 
     @DisplayName("Тест загрузки изображения")
@@ -45,90 +58,74 @@ public class ImgurApiTest {
     @Order(2)
     void testUploadImage() {
         String url = "/image";
+        ResponseSpecification responseSpecification = new ResponseSpecBuilder()
+                .expectBody("data.id", is(notNullValue()))
+                .build();
         given().when()
-                .auth()
-                .oauth2(ImgurApiParams.TOKEN)
+                .spec(requestSpecification)
                 .log()
                 .all()
                 .multiPart("image", FILE_URL)
                 .expect()
-                .log()
-                .all()
-                .body("success", is(true))
-                .body("data.id", is(notNullValue()))
-                .contentType("application/json")
+                .spec(responseSpecification)
                 .when()
                 .post(url)
-                .then()
-                .statusCode(200);
-                //.prettyPeek()
-                //.extract()
-                //.response()
-                //.jsonPath()
-                //.getString("data.id");
-
+                .prettyPeek();
     }
     @DisplayName("Тест получения изображения")
     @Test
     @Order(3)
     void testGetImage() {
-        String imageHash = "TGtQxL4";
+        String imageHash = "9nZOI7z";
         String url = "image/" + imageHash;
+        ResponseSpecification responseSpecification = new ResponseSpecBuilder()
+                .expectBody("data.id", is(imageHash))
+                .build();
         given().when()
-                .auth()
-                .oauth2(ImgurApiParams.TOKEN)
+                .spec(requestSpecification)
                 .log()
                 .all()
                 .expect()
-                .statusCode(is(200))
-                .body("status", is(200))
-                .body("success", is(true))
-                .body("data.id", is(imageHash))
-                .log()
-                .all()
+                .spec(responseSpecification)
                 .when()
-                .get(url);
+                .get(url)
+                .prettyPeek();
     }
     @DisplayName("Тест обновления информации о изображении")
     @Test
     @Order(4)
     void testUpdateImageInfo() {
-        String imageHash = "TGtQxL4";
+        String imageHash = "9nZOI7z";
         String url = "image/" + imageHash;
+        RequestSpecification requestSpecification = new RequestSpecBuilder()
+                .addFormParam("title", "Jhon Bon Jovi")
+                .addFormParam("description", "My favorite car")
+                .build();
         given().when()
-                .auth()
-                .oauth2(ImgurApiParams.TOKEN)
+                .spec(requestSpecification)
                 .log()
                 .all()
-                .formParam("title", "Jhon Bon Jovi")
-                .formParam("description", "My favorite car")
                 .expect()
-                .log()
-                .all()
-                .statusCode(is(200))
-                .body("success", is(true))
-                .body("data", is(true))
+                .spec(responseSpecification)
                 .when()
-                .post(url);
+                .post(url)
+                .prettyPeek();
     }
-    @DisplayName("Тест метки любимое изображение")
+    @DisplayName("Тест добавления изображения в избранное")
     @Test
     @Order(5)
     void testFavoriteAnImage() {
-        String imageHash = "TGtQxL4";
+        String imageHash = "9nZOI7z";
         String url = "image/" + imageHash + "/favorite";
         given().when()
-                .auth()
-                .oauth2(ImgurApiParams.TOKEN)
+                .spec(requestSpecification)
                 .log()
                 .all()
                 .expect()
-                .statusCode(is(200))
-                .body("success", is(true))
-                .log()
-                .all()
+                .spec(responseSpecification)
                 .when()
-                .post(url);
+                .post(url)
+                .prettyPeek();
     }
 
     @DisplayName("Тест добавления альбома")
@@ -136,46 +133,38 @@ public class ImgurApiTest {
     @Order(6)
     void createAlbumTest() {
         String url = "album";
+        RequestSpecification requestSpecification = new RequestSpecBuilder()
+                .addFormParam("title", "My first album")
+                .addFormParam("description", "This album is cool")
+                .build();
+        ResponseSpecification responseSpecification = new ResponseSpecBuilder()
+                .expectBody("data.id", is(notNullValue()))
+                .build();
         given().when()
-                .auth()
-                .oauth2(ImgurApiParams.TOKEN)
+                .spec(requestSpecification)
                 .log()
                 .all()
-                .formParam("title", "My first album")
-                .formParam("description", "This album is cool")
                 .expect()
-                .body("success", is(true))
-                .body("data.id", is(notNullValue()))
-                .contentType("application/json")
+                .spec(responseSpecification)
                 .when()
                 .post(url)
-                .prettyPeek()
-                .then()
-                .statusCode(200);
-                //.extract()
-                //.response()
-                //.jsonPath()
-                //.getString("data.deletehash");
+                .prettyPeek();
     }
 
     @DisplayName("Тест удаления альбома")
     @Test
     @Order(7)
     void deleteAlbumTest() {
-        String albumHash = "7hJbXDT";
+        String albumHash = "nOgRaZZ";
         String url = "album/" + albumHash;
         given().when()
-                .auth()
-                .oauth2(ImgurApiParams.TOKEN)
+                .spec(requestSpecification)
                 .log()
                 .all()
+                .expect()
+                .spec(responseSpecification)
+                .when()
                 .delete(url)
-                .prettyPeek()
-                .then()
-                .log()
-                .all()
-                .statusCode(200);
+                .prettyPeek();
     }
-
-
 }
